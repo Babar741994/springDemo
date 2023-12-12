73ce5a4c77f9d5ddec3d1dd5a348f0635179440e
BabarAli <BabarAli@DESKTOP-EQ7J7VJ>
2023-12-12 16:14:21 +0500

    Persisting Data with Spring Data JPA

--- springDemoProject/src/main/java/tacos/web/OrderController.java
+++ springDemoProject/src/main/java/tacos/web/OrderController.java
@@ -1,4 +1,4 @@
-package tacos;
+package tacos.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -10,12 +10,20 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import tacos.TacoOrder;
+import tacos.data.OrderRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+	private OrderRepository orderRepo;
+	
+	public OrderController (OrderRepository orderRepo) {
+		this.orderRepo = orderRepo;
+	}
+	
 	
 	@GetMapping("/current")
 	public String orderForm() {
@@ -28,7 +36,7 @@ public class OrderController {
 		if(errors.hasErrors()) {
 			return "orderForm";
 		}
-		log.info("Order submitted: {}", order);
+		orderRepo.save(order);
 		sessionStatus.setComplete();
 		return "redirect:/";
 	}
